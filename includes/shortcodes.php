89deef624d65e4d3579fc3860a6d6a9576cb8b25
aliakro <aliakro@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-13 20:33:41 +0000

    Initial commit of plugin to SVN. Version 1.3
    git-svn-id: https://plugins.svn.wordpress.org/weight-loss-tracker/trunk@1044127 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/shortcodes.php
@@ -0,0 +1,452 @@
+<?php
+	defined('ABSPATH') or die("Jog on!");
+
+	function ws_ls_shortcode()
+	{
+		$output = "";
+
+		/* $a = shortcode_atts( array(
+        'user_id' => 'default_value',
+        'another_one' => 'default_value',
+   		 ), $atts );
+		*/
+
+
+		if (!is_user_logged_in())
+		{
+			$output .= "
+							<blockquote><p>
+					        	You need to be logged in to record your weight.
+					    	</p></blockquote>
+						";
+		}
+		else
+		{
+			if ($_POST)
+			{
+				if (ws_ls_save_data())
+				{
+					$output .= "
+									<blockquote><p>Saved!</p></blockquote>
+							    	
+								";
+
+				}
+				
+			}
+
+			$user_data = ws_ls_get_weights(get_current_user_id());
+
+			if (is_array($user_data) && count($user_data) > 1)
+			{
+				$output .= ws_ls_title("In a chart");
+
+				$user_data_limited = ws_ls_get_weights(get_current_user_id(), 30);
+
+				$output .= ws_ls_display_chart($user_data_limited);
+			}
+			else
+			{
+					$output .= "
+									<blockquote><p>
+							        	A pretty graph shall appear once you have recorded several weights.
+							    	</p></blockquote>
+								";
+			}
+			$output .= ws_ls_title("Add a new weight");
+			$output .= ws_ls_display_form();
+
+			if (is_array($user_data) && count($user_data) > 0)
+			{
+				$output .= ws_ls_title("Weight History");
+
+				$output .= ws_ls_display_table($user_data);
+			}
+		}
+		
+		return $output;
+		
+	}
+
+	
+
+
+	function ws_ls_display_form()
+	{
+		wp_enqueue_script('jquery-ui-datepicker');
+		
+		wp_enqueue_style('jquery-style', plugins_url( 'css/jquery-ui.css', __FILE__ ));
+		
+		wp_enqueue_script(
+			'jquery-validate',
+			plugins_url( 'js/jquery.validate.min.js', __FILE__ )		
+		);
+		
+		$form_class = (WE_LS_SUPPORT_AVADA_THEME) ? "avada-contact-form" :"ws_ls_display_form";
+		
+		$output = "
+
+		<form action=\"" .  get_site_url() . "/" . get_query_var('pagename') . "\" method=\"post\" class=\"" . $form_class .  "\" id=\"weight_form\">
+			<input type=\"hidden\" value=\"\" name=\"weight_user_id\">
+			<div id=\"comment-input\">
+				<input type=\"date\" name=\"weight_date\" id=\"weight_date\" value=\"" . date("Y-m-d") . "\" placeholder=\"" . date("Y-m-d") . "\" size=\"22\" tabindex=\"1\">";
+			
+			if(WE_LS_IMPERIAL_WEIGHTS)
+			{
+				$output .= "<input type=\"text\" name=\"weight_stones\" id=\"weight_stones\" value=\"\" placeholder=\"Stones\" size=\"11\" tabindex=\"2\" >";
+				$output .= "<input type=\"text\" name=\"weight_pounds\" id=\"weight_pounds\" value=\"\" placeholder=\"Pounds\" size=\"11\" tabindex=\"3\" >";
+			}
+			else
+				$output .= "<input type=\"text\" name=\"weight_weight\" id=\"weight_weight\" value=\"\" placeholder=\"Weight (Kg)\" size=\"22\" tabindex=\"2\">";
+				
+			$output .= "</div>
+			<div id=\"comment-textarea\">
+				<textarea name=\"weight_notes\" id=\"weight_notes\" cols=\"39\" rows=\"4\" tabindex=\"4\" class=\"textarea-comment\" placeholder=\"Notes\"></textarea>
+			</div>
+			<div id=\"comment-submit-container\">
+				<p>
+					<div>
+						<input name=\"submit_button\" type=\"submit\" id=\"submit_button\" tabindex=\"5\" value=\"Save Entry\" class=\"comment-submit btn btn-default button default small fusion-button button-small button-default button-round button-flat\">
+					</div>
+				</p>
+			</div>
+		</form>
+		<script>
+			jQuery(document).ready(function() {
+		
+				jQuery(\"#weight_form\").validate({
+                rules: {
+                    weight_date:  {
+                        required: true,
+                        date: true
+                    },";
+
+                    if(WE_LS_IMPERIAL_WEIGHTS)
+					{
+						 $output .= " weight_stones:  {
+				                        required: true,
+				                        number: true
+				                    },
+				                    weight_pounds:  {
+				                        required: true,
+				                        number: true
+				                    },";
+
+					}
+					else
+					{
+						 $output .= " weight_weight:  {
+				                        required: true,
+				                        number: true
+				                    }";
+					}                 
+                   
+                $output .= "},
+                messages: {
+                    weight_weight: \"Please enter your weight\",
+                  	weight_date: \"Please enter the date\",
+                },
+                submitHandler: function(form) {
+                    form.submit();
+                }
+            });
+
+			});
+		</script>
+
+		";
+
+		return $output;
+
+	}
+	function ws_ls_display_table($data)
+	{
+		$output = "";
+
+		if (WE_LS_SUPPORT_AVADA_THEME) 
+				$output .= "<div class=\"fusion-table table-2\">";
+
+		$unit = (WE_LS_IMPERIAL_WEIGHTS) ? "St lbs" : "Kg";
+
+		$output .= "<table  width=\"100%\">
+		<thead>
+		<tr>
+			
+			<th>Date</th>
+			<th>Weight (" . $unit . ")</th>
+			<th>Notes</th>
+		</tr>
+		</thead>
+		<tbody>";
+	
+		foreach ($data as $row)
+		    {
+
+
+		    	if(WE_LS_IMPERIAL_WEIGHTS)
+		    		$weight = $row->weight_stones . "st " . $row->weight_pounds . "lbs";
+		    	else
+		    		$weight = $row->weight_weight;
+
+		$output .= "<tr>
+				<td>" . $row->weight_date_formatted . "</td>
+				<td>" . $weight . "</td>
+				<td>" . esc_html($row->weight_notes) . "</td>
+			</tr>";
+
+		}
+
+		$output .= "<tbody></table>";
+
+		if (WE_LS_SUPPORT_AVADA_THEME) 
+			$output .= "</div>";
+
+		return $output;
+	}
+	
+	function ws_ls_display_chart($data)
+	{
+
+
+
+ 		$output = "<script src=\"". plugins_url( 'js/Chart.min.js', __FILE__ ) . "\" type=\"text/javascript\"></script>";
+
+ 		$output .= "<canvas id=\"myChart\" ></canvas>
+
+ 		<script>
+		var ctx = document.getElementById('myChart').getContext('2d');
+
+ 		var data = {
+		    labels: [";
+
+		    foreach ($data as $row)
+		    {
+
+		    	$output .= "'" . $row->weight_date_formatted . "',";		    
+		    }
+
+		$output .= "], 
+				datasets: [
+		        {
+		            label: 'Weight',
+		            fillColor: 'rgba(220,220,220,0.2)',
+		            strokeColor: 'rgba(220,220,220,1)',
+		            pointColor: 'rgba(220,220,220,1)',
+		            pointStrokeColor: '#fff',
+		            pointHighlightFill: '#fff',
+		            pointHighlightStroke: 'rgba(220,220,220,1)',
+		            data: [";
+   			
+   			foreach ($data as $row)
+		    {
+		    	if(WE_LS_IMPERIAL_WEIGHTS)
+		    		$output .= "'" . $row->weight_stones . "." . $row->weight_pounds . "',";
+		    	else
+		    		$output .= "'" . $row->weight_weight . "',";
+	    
+		    }
+
+		$output .= "]
+		        }
+		    
+		    ]
+		};
+
+		var options = 
+		{
+
+		    ///Boolean - Whether grid lines are shown across the chart
+		    scaleShowGridLines : true,
+
+		    //String - Colour of the grid lines
+		    scaleGridLineColor : 'rgba(0,0,0,.05)',
+
+		    //Number - Width of the grid lines
+		    scaleGridLineWidth : 1,
+			 scaleOverride: false,
+
+			    // ** Required if scaleOverride is true **
+			    // Number - The number of steps in a hard coded scale
+			    scaleSteps: 14,
+			    // Number - The value jump in the hard coded scale
+			    scaleStepWidth: 10,
+			    // Number - The scale starting value
+			    scaleStartValue: 20,
+		    //Boolean - Whether the line is curved between points
+		    bezierCurve : true,
+
+		    //Number - Tension of the bezier curve between points
+		    bezierCurveTension : 0.4,
+
+		    //Boolean - Whether to show a dot for each point
+		    pointDot : true,
+
+		    //Number - Radius of each point dot in pixels
+		    pointDotRadius : 4,
+
+		    //Number - Pixel width of point dot stroke
+		    pointDotStrokeWidth : 1,
+
+		    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+		    pointHitDetectionRadius : 20,
+
+		    //Boolean - Whether to show a stroke for datasets
+		    datasetStroke : true,
+
+		    //Number - Pixel width of dataset stroke
+		    datasetStrokeWidth : 2,
+
+		    //Boolean - Whether to fill the dataset with a colour
+		    datasetFill : true,
+
+		    //String - A legend template
+		    legendTemplate : '<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>'
+
+		};
+
+
+ 		var width = jQuery('#myChart').parent().width();
+		jQuery('#myChart').attr(\"width\",width);
+		new Chart(ctx).Line(data,options);
+		window.onresize = function(event){
+		    var width = jQuery('#myChart').parent().width();
+		    jQuery('#myChart').attr(\"width\",width);
+		    new Chart(ctx).Line(data,options);
+		};
+
+ 		</script>
+
+
+ 		";
+
+
+ 		return $output;
+	}
+
+	function ws_ls_get_weights($user_id, $limit = 100)
+	{
+		global $wpdb;
+
+   		$table_name = $wpdb->prefix . WE_LS_TABLENAME;
+
+   		$sql =  $wpdb->prepare("SELECT DATE_FORMAT(weight_date,'%%d %%b') as weight_date_formatted, weight_date, weight_weight, weight_stones, weight_pounds, weight_notes FROM $table_name where weight_user_id = %d order by weight_date limit 0, %d", $user_id,  $limit);
+
+		$rows = $wpdb->get_results( $sql );
+
+		return $rows;
+
+	}
+
+	function ws_date_exists($user_id, $date)
+	{
+		global $wpdb;
+
+   		$table_name = $wpdb->prefix . WE_LS_TABLENAME;
+
+   		$sql =  $wpdb->prepare("SELECT count(*) as iCount FROM " . $table_name . " WHERE weight_date = %s and weight_user_id = %d", $date, $user_id);
+
+		$rows = $wpdb->get_row($sql);
+
+		if ($rows->iCount == 0)
+			return false;
+		else
+			return true;
+	}
+
+	function ws_ls_save_data()
+	{
+
+		if ( $_POST )
+		{
+			global $wpdb;
+
+			$table_name = $wpdb->prefix . WE_LS_TABLENAME;
+
+			$values = stripslashes_deep($_POST);
+			unset($values["submit_button"]);
+	
+			$values["weight_user_id"] = get_current_user_id();
+
+			// Convert stones / lbs to Kg
+ 			if(WE_LS_IMPERIAL_WEIGHTS)
+				$values["weight_weight"] =  ws_ls_to_kg($values["weight_stones"], $values["weight_pounds"] );
+
+			if (!ws_date_exists($values["weight_user_id"], $values["weight_date"]))
+			{
+	
+				$wpdb->insert( 
+					$table_name, 
+					$values
+				);
+
+				return true;
+			}
+			else
+			{
+				if(!WE_LS_IMPERIAL_WEIGHTS)
+					$sql = $wpdb->prepare("Update " . $table_name. " Set weight_notes = %s, weight_weight = %f where weight_user_id = %d and weight_date = %s", $values['weight_notes'], $values["weight_weight"], $values["weight_user_id"], $values["weight_date"]);
+				else
+					$sql = $wpdb->prepare("Update " . $table_name. " Set weight_notes = %s, weight_weight = %f, weight_stones = %f, weight_pounds = %f where weight_user_id = %d and weight_date = %s", $values['weight_notes'], $values["weight_weight"], $values["weight_stones"], $values["weight_pounds"], $values["weight_user_id"], $values["weight_date"]);
+		
+				$wpdb->query($sql);
+			
+				return true;
+			}
+			
+		}
+	
+		return false;
+
+	}
+	
+
+function ws_ls_register_shortcodes()
+{
+ 	add_shortcode( 'weightlosstracker', 'ws_ls_shortcode' );
+}
+
+function ws_ls_title($text)
+{
+	if(WE_LS_SUPPORT_AVADA_THEME)
+		return "<div class=\"fusion-title title\"><h2 class=\"title-heading-left\">" . $text . "</h2><div class=\"title-sep-container\"><div class=\"title-sep sep-single sep-dashed\"></div></div></div>";
+	else
+		return "<h2 class=\"ws_ls_title\">" . $text . "</h2>";
+
+}
+
+function ws_ls_to_kg($stones, $pounds)
+{
+	$pounds += $stones * 14;
+	
+	return round($pounds / 2.2, 2);
+}
+
+function ws_ls_to_lb($kg)
+{
+	$pounds = $kg * 2.2;
+	
+	return round($pounds, 2);
+}
+
+function ws_ls_to_stones($pounds)
+{
+	$pounds = $pounds / 14;
+	
+	return round($pounds, 2);
+}
+
+function ws_ls_to_stone_pounds($kg)
+{
+
+	$weight = array ("Stones" => 0, "Pounds" => 0);
+
+    $totalPounds = Round($kg * 2.20462, 3);
+
+    $weight["Stones"] = round($totalPounds / 14, 2);
+    $weight["Pounds"] = Round($totalPounds - ($weight["Pounds"] * 14), 0);
+
+    return $weight;
+
+}
+
+?>
