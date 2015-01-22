414b81950b8d25ed44d8e6738203ca792fad548b
aliakro <aliakro@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-22 15:15:53 +0000

    1.5 Release
    git-svn-id: https://plugins.svn.wordpress.org/weight-loss-tracker/tags/1.5@1073415 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ installer/activate.php
@@ -0,0 +1,32 @@
+<?php
+
+	defined('ABSPATH') or die("Jog on!");
+
+
+	function ws_ls_activate()
+	{
+		$debug_values = true;
+
+		global $wpdb;
+
+   		$table_name = $wpdb->prefix . WE_LS_TABLENAME;
+		
+
+		$charset_collate = $wpdb->get_charset_collate();
+
+		$sql = "CREATE TABLE $table_name (
+		  id mediumint(9) NOT NULL AUTO_INCREMENT,
+		  weight_date datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+		  weight_user_id integer NOT NULL,
+		  weight_weight decimal NOT NULL,
+		  weight_stones decimal NOT NULL,
+		  weight_pounds decimal NOT NULL,
+		  weight_notes text null,
+		  UNIQUE KEY id (id)
+		) $charset_collate;";
+
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $sql );
+	}
+
+?>
