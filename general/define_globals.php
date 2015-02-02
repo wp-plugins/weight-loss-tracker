e2abfb1ac50087fae70e515f9eb212066f0bee10
aliakro <aliakro@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-02 08:44:13 +0000

    1.5.1 minor release
    git-svn-id: https://plugins.svn.wordpress.org/weight-loss-tracker/tags/1.5.1@1080696 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ general/define_globals.php
@@ -0,0 +1,11 @@
+<?php
+
+	// -----------------------------------------------------------------------------------------
+	// AC: Define Global Variables
+	// -----------------------------------------------------------------------------------------
+	
+	define('WE_LS_TITLE', "Weight Loss Tracker");
+	define('WE_LS_TABLENAME', "WS_LS_DATA");
+	define('WE_LS_SUPPORT_AVADA_THEME', false);
+	define('WE_LS_IMPERIAL_WEIGHTS', true);
+?>
---
+++ general/register_hooks.php
@@ -0,0 +1,7 @@
+<?php
+
+	defined('ABSPATH') or die("Jog on!");
+
+	add_action( 'init', 'ws_ls_register_shortcodes');
+
+?>
